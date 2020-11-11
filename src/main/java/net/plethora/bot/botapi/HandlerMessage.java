69139712c7fa255491885a1b1ec46fb5f54b6c0a
Rebel-cat <54984087+pal4d1n@users.noreply.github.com>
2020-11-11 19:06:46 +0300

    add HandlerMSg

---
+++ src/main/java/net/plethora/bot/botapi/HandlerMessage.java
@@ -0,0 +1,103 @@
+package net.plethora.bot.botapi;
+
+import net.plethora.bot.dao.DataAccessObject;
+import net.plethora.bot.model.Answer;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class HandlerMessage {
+
+    private Answer answer;
+    private final String NEGATIVE_ANSWER = "хм... Про это ничего не знаю";
+    private final DataAccessObject dao;
+
+    public HandlerMessage(DataAccessObject dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * Получаем сообщение из базы для клиента по запросу
+     *
+     * @param ask сообщение запроса
+     * @return String содержащий ответ
+     */
+    private String getStrAnswer(String ask) {
+        answer = assignAnswer(ask);  //присваиваем объект из mongodb
+        return answer != null ? answer.getAnswer() : NEGATIVE_ANSWER;
+    }
+
+    /**
+     * Делаем запрос в базу данных
+     *
+     * @param ask строка запроса
+     */
+    private Answer assignAnswer(String ask) {
+        return dao.handleRequest(ask);//ищем в базе через метод репозитория и присваиваем документ
+    }
+
+    /**
+     * Формируем сообщение
+     *
+     * @param chatId id сообщения запроса
+     * @param ask    строка запроса
+     * @return sendMessage готовое сообщение
+     */
+    public SendMessage sendMessage(long chatId, String ask) {
+        SendMessage sendMessage = new SendMessage(chatId, getStrAnswer(ask));
+        if (answer != null && answer.getKeyWords().length > 0) {
+            sendMessage.setReplyMarkup(getInlineKeyboardMarkup());
+        } //добавляем клавиатуру
+        return sendMessage;
+    }
+
+    /**
+     * Создаем клавиатуру под сообщением на основе ключевых слов в документе Mongodb
+     *
+     * @return keyboardMsg  - готовая клавиатура
+     */
+    private InlineKeyboardMarkup getInlineKeyboardMarkup() {
+
+        InlineKeyboardMarkup keyBoardMsg = new InlineKeyboardMarkup();   //наша клава под msg
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>(); //формируем ряды
+
+        fillRows(rows);
+
+        int countGlobal = 0; //счетчик всех по порядку ключевых слов
+        for (List<InlineKeyboardButton> list : rows) {
+            int count = 0;  //внутренний счетчик на количество кнопок в ряду
+            while (countGlobal < answer.getKeyWords().length) {
+                if (count + 1 > 3) {
+                    break;
+                }   //если 4 кнопки добавлены выходим
+                InlineKeyboardButton button = new InlineKeyboardButton(answer.getKeyWords()[countGlobal]);    //текст на кнопке
+                button.setCallbackData(answer.getKeyWords()[countGlobal]); //текст содержащийся в колбеке
+                count++;
+                countGlobal++;
+                list.add(button);
+            }
+        }
+
+        keyBoardMsg.setKeyboard(rows);
+
+        return keyBoardMsg;
+    }
+
+    /**
+     * Наполняем массив пустыми рядами
+     *
+     * @param rows
+     */
+    private void fillRows(List<List<InlineKeyboardButton>> rows) {
+        int numberRows = (answer.getKeyWords().length / 3) + 1;//делим на 3 и округляем в большую сторону
+        for (int i = 0; i < numberRows; i++) {   //заполняем лист пустыми рядами
+            rows.add(new ArrayList<>());
+        }
+    }
+}
