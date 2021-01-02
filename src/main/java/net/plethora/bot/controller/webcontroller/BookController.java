2befd0e79b84f60b370cfc40c6ed420f4e40bbf1
Rebel-cat <54984087+pal4d1n@users.noreply.github.com>
2021-01-02 16:08:55 +0300

    create book for book page

--- src/main/java/net/plethora/bot/controller/webcontroller/BookController.java
+++ src/main/java/net/plethora/bot/controller/webcontroller/BookController.java
@@ -23,6 +23,8 @@ public class BookController {
         Book book = dataAccessMaterialBook.findById(id);
         List<Book> books = dataAccessMaterialBook.findAll();
 
+        model.addAttribute("name", book.getName());
+        model.addAttribute("author", book.getAuthor());
         model.addAttribute("idBook", book.getId());
         model.addAttribute("urlBook", book.getUrl());
         model.addAttribute("urlCover", book.getUrlCoverBook());
@@ -34,12 +36,15 @@ public class BookController {
     }
 
     @RequestMapping(value = "/book/edit", method = RequestMethod.POST, params = "action=update")
-    public String update(String idCoverBook, String idBook, String subject,
-                         String description, Model model) {
+    public String update(String urlCoverBook, String idBook, String subject, String urlBook,
+                         String description, Model model, String name, String author) {
         Book book = dataAccessMaterialBook.findById(idBook);
-        book.setUrlCoverBook(idCoverBook);
+        book.setUrlCoverBook(urlCoverBook);
+        book.setUrl(urlBook);
         book.setSubject(subject);
         book.setDescription(description);
+        book.setAuthor(author);
+        book.setName(name);
         dataAccessMaterialBook.save(book);
 
         List<Book> books = dataAccessMaterialBook.findAll();
@@ -50,6 +55,24 @@ public class BookController {
     @RequestMapping(value = "/book/edit", method = RequestMethod.POST, params = "action=delete")
     public String delete(String idBook, Model model) {
         dataAccessMaterialBook.delete(idBook);
+        List<Book> books = dataAccessMaterialBook.findAll();
+        model.addAttribute("books", books);
+        return "book";
+    }
+
+    @RequestMapping(value = "/book/edit", method = RequestMethod.POST, params = "action=create")
+    public String create(String urlCoverBook, String urlBook, String subject,
+                         String description, Model model, String name, String author) {
+        Book book = new Book();
+
+        book.setUrlCoverBook(urlCoverBook);
+        book.setUrl(urlBook);
+        book.setSubject(subject);
+        book.setDescription(description);
+        book.setName(name);
+        book.setAuthor(author);
+        dataAccessMaterialBook.save(book);
+
         List<Book> books = dataAccessMaterialBook.findAll();
         model.addAttribute("books", books);
         return "book";
